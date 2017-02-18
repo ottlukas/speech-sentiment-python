e315239a521bba1d452d207d576c771d6bb87b6e
Lukas Ott <Lukas Ott>
2017-02-18 13:56:24 +0100

    Sentimet to Audio added .env adjusted to handle both TexttoSpeech and SpeechtoTExt

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # speech-sentiment-python
 polite software
-
+and sentiment to speech
 
 ## .env
-BLUEMIX_USERNAME=*your bluemix credential*
-
-BLUEMIX_PASSWORD=*your bluemix credential*
-
+SPEECH_TO_TEXT_USERNAME=*your bluemix credential*
+SPEECH_TO_TEXT_PASSWORD=*your bluemix credential*
+TEXT_TO_SPEECH_USERNAME=*your bluemix credential*
+TEXT_TO_SPEECH_PASSWORD=*your bluemix credential*
 ALCHEMY_API_KEY=*your alchemy api key*
--- run.py
+++ run.py
@@ -4,18 +4,63 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from watson_developer_cloud import SpeechToTextV1 as SpeechToText
 from watson_developer_cloud import AlchemyLanguageV1 as AlchemyLanguage
+import pyaudio
+import wave
+from watson_developer_cloud import TextToSpeechV1
 
 from speech_sentiment_python.recorder import Recorder
 
+            
+
 def transcribe_audio(path_to_audio_file):
-    username = os.environ.get("BLUEMIX_USERNAME")
-    password = os.environ.get("BLUEMIX_PASSWORD")
+    username = os.environ.get("SPEECH_TO_TEXT_USERNAME")
+    password = os.environ.get("SPEECH_TO_TEXT_PASSWORD")
     speech_to_text = SpeechToText(username=username,
                                   password=password)
 
     with open(join(dirname(__file__), path_to_audio_file), 'rb') as audio_file:
         return speech_to_text.recognize(audio_file,
             content_type='audio/wav')
+    
+def synthesize_audio(text):
+
+    text_to_speech = TextToSpeechV1(
+    username=os.environ.get("TEXT_TO_SPEECH_USERNAME"),
+    password=os.environ.get("TEXT_TO_SPEECH_PASSWORD"),
+    x_watson_learning_opt_out=True)  # Optional flag
+
+    with open(join(dirname(__file__), 'output.wav'),
+          'wb') as audio_file:
+        audio_file.write(
+            text_to_speech.synthesize(text, accept='audio/wav',
+                                      voice="en-US_AllisonVoice"))
+
+    #define stream chunk   
+    chunk = 1024  
+
+    #open a wav format music  
+    f = wave.open(r"output.wav","rb")  
+    #instantiate PyAudio  
+    p = pyaudio.PyAudio()  
+    #open stream  
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+                    channels = f.getnchannels(),  
+                    rate = f.getframerate(),  
+                    output = True)  
+    #read data  
+    data = f.readframes(chunk)  
+
+    #play stream  
+    while data:  
+        stream.write(data)  
+        data = f.readframes(chunk)  
+
+    #stop stream  
+    stream.stop_stream()  
+    stream.close()  
+
+    #close PyAudio  
+    p.terminate()  
 
 def get_text_sentiment(text):
     alchemy_api_key = os.environ.get("ALCHEMY_API_KEY")
@@ -23,7 +68,7 @@ def get_text_sentiment(text):
     alchemy_language = AlchemyLanguage(api_key=alchemy_api_key)
     result = alchemy_language.sentiment(text=text)
     if result['docSentiment']['type'] == 'neutral':
-        return 'netural', 0
+        return 'neutral', 0
     return result['docSentiment']['type'], result['docSentiment']['score']
 
 def main():
@@ -39,7 +84,9 @@ def main():
     print("Text: " + text + "\n")
     
     sentiment, score = get_text_sentiment(text)
-    print(sentiment, score)  
+    print(sentiment, score)
+    output = str(sentiment + ' '+score)
+    synthesize_audio(output)
 
 if __name__ == '__main__':
     dotenv_path = join(dirname(__file__), '.env')
